4a28447c33dfbab1616fe63efd20f0f6da6e8c84
Juan Bruzigues <jabruzigues@gmail.com>
2025-05-17 00:28:53 -0300

    Update cartadeamor.jsx

--- src/components/cartadeamor.jsx
+++ src/components/cartadeamor.jsx
@@ -4,7 +4,7 @@ import './cartadeamor.scss';
 const CartaDeAmor = () => {
   return (
     <div className = "carta">
-      <p>Meu amor,</p>
+      <hr></hr>
       <p>Já faz um ano desde que começamos essa jornada juntos, e cada dia ao seu lado me mostra o quanto você é essencial para mim. Te amar é encontrar paz e força ao mesmo tempo, é querer estar perto em todos os momentos, mesmo nos silenciosos.</p>
       <p>Obrigado por ser meu porto seguro e minha alegria. Que venham muitos anos assim do seu lado.</p>
       <p>Com todo carinho,</p>
