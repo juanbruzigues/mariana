cd03c57ccaba127ae568828f0d21804d66f6d35a
Juan Bruzigues <jabruzigues@gmail.com>
2025-05-17 00:59:34 -0300

    Atualizar o cartadeamor.jsx

--- src/components/cartadeamor.jsx
+++ src/components/cartadeamor.jsx
@@ -7,7 +7,7 @@ const CartaDeAmor = () => {
       <p>A vida me fez amadurecer, mas nunca me fez esquecer.
         Você sempre foi aquele amor que ficou mesmo com o tempo, mesmo com a distância, mesmo com as mudanças.</p>
       <p>“Tô mais maduro e mais seguro do que eu quero…”
-        Ganhei algumas marcas, perdi um pouco do med e descobri que o amor de verdade não passa. Ele cresce.</p>
+        Ganhei algumas marcas, perdi um pouco do medo e descobri que o amor de verdade não passa. Ele cresce.</p>
       <p>Com todo amor,</p>
       <p>Juan</p>
     </div>
